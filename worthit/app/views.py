d225421aedcc57ca29ce47d799c2480d95913ac0
Martin v. Löwis <loewis@beuth-hochschule.de>
2014-12-22 20:14:04 +0100

    Fix path names.

--- worthit/app/views.py
+++ worthit/app/views.py
@@ -7,25 +7,25 @@ def hello(request):
     return HttpResponse('Hello World!')
 
 def home(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\home.html')
+    return render_to_response('home.html')
 
 def mobileRate(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\mobileRate.html')
+    return render_to_response('mobileRate.html')
 
 def TVrate(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\TVrate.html')
+    return render_to_response('TVrate.html')
 
 def booksRate(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\\booksRate.html')
+    return render_to_response('booksRate.html')
 
 def mobileView(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\mobileView.html')
+    return render_to_response('mobileView.html')
 
 def TVview(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\TVview.html')
+    return render_to_response('TVview.html')
 
 def booksView(request):
-    return render_to_response('C:\Program Files\Code\Internettechnologie\worthit\pagefiles\\booksView.html')
+    return render_to_response('booksView.html')
 
 def add_form_data(request):
     success = 'Your name is: ' + request.GET['vorname'] + request.GET['nachname']+'Your Email: '+request.GET['email']+'Your Category: '+request.GET['kategorie']+'Your note: '+request.GET['note']+'Your message: '+request.GET['beschreibung']
